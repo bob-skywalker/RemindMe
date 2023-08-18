4311c2719b04291b29a5d9ba46e296f9d2d8414c
bobo <q463250938@gmail.com>
2023-08-18 12:38:21 -0700

    Initial Commit

---
+++ RemindMe/RemindMeApp.swift
@@ -0,0 +1,17 @@
+//
+//  RemindMeApp.swift
+//  RemindMe
+//
+//  Created by Bo Zhong on 8/18/23.
+//
+
+import SwiftUI
+
+@main
+struct RemindMeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
