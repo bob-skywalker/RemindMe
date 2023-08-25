e158b438cc1c97b8ef5fcabbfc14034de86760e6
bobo <q463250938@gmail.com>
2023-08-25 12:07:03 -0700

    firebaseStore init in the app level , now app is connect to firebase backend

--- RemindMe/RemindMeApp.swift
+++ RemindMe/RemindMeApp.swift
@@ -6,9 +6,15 @@
 //
 
 import SwiftUI
+import FirebaseCore
 
 @main
 struct RemindMeApp: App {
+    
+    init(){
+        FirebaseApp.configure()
+    }
+    
     var body: some Scene {
         WindowGroup {
             ContentView()
